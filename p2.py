08bba5be9dd70de17cbf9afaaf426ee43d926319
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-07 17:36:22 +0530

    .

---
+++ p2.py
@@ -0,0 +1,13 @@
+# Declaring variables
+name = 'John'
+age = 25
+print("My name is", name, "and I am", age, "years old.")
+
+
+
+# Basic operations
+sum = 5 + 3
+minus = 10 - 4
+
+print('Sum:', sum)
+print('Minus:', minus)
