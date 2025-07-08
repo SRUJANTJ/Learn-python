7b90ed02ea12d22b76e363b2e593cfc1aee876f5
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-08 18:27:36 +0530

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,19 @@
+# Datatypes : Text Type
+
+# Storing a String
+name = 'Alice'
+# Using Triple Quotes
+multiline_string = '''This is a
+multiline string'''
+
+print(name);
+print(multiline_string);
+
+# Numeric Type
+
+# Storing an Integer
+a = 10;
+#printing the 
+print(a);
+#Type check
+print(type(a));
