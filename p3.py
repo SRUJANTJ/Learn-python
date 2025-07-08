6d5e39170dcf7e529450535eba39a1536d6f85dd
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-08 18:59:37 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -17,3 +17,43 @@ a = 10;
 print(a);
 #Type check
 print(type(a));
+
+
+# List Type
+
+# Storing a List
+my_list = [1, 'two', 3.0, True]
+#Printing my_list
+print(my_list)
+#Checking the type
+print(type(my_list))
+
+
+# Tuple Type
+# Storing a Tuple
+my_tuple = (1, 'two', 3.0)
+#Printing my_tuple
+print(my_tuple)
+#Checking the type
+print(type(my_tuple))
+
+
+
+# Range
+
+
+
+#One argument
+for i in range(5):
+ print("Once argument",i)
+# Output: 0, 1, 2, 3, 4 
+
+#Two argument
+for i in range(2, 6):
+ print("Two argument",i)
+# Output: 2, 3, 4, 5
+
+#Three argument
+for i in range(1, 10, 2):
+ print("Three argument",i)
+# Output: 1, 3, 5, 7, 9
