98ef9da77c66084b9351428e4bc134825385d872
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-09 19:57:11 +0530

    .

---
+++ D4.py
@@ -0,0 +1,21 @@
+# Storing a Dictionary
+my_dict = {'name': 'Alice', 'age': 25, 'is_student': True}
+print(my_dict) 
+#Type Chek 
+print(type(my_dict))
+#Output {'name': 'Alice', 'age': 25, 'is_student': True} 
+
+
+# Set Type
+
+# Storing a Set
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+#Type check
+print(type(my_set))
+#Output {1, 2, 3, 4, 5}
+
+# Storing a Duplicate Set
+dupclite_set = {1, 5,5,5}
+print(dupclite_set)
+#Output {1, 5} 
