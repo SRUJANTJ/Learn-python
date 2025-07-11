20922cf5488ebf5592e9c0b6f67f4aa5bb0fc5d4
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-11 18:06:32 +0530

    Update D6.py

---
+++ D6.py
@@ -0,0 +1,17 @@
+#Arithmetic Operators
+a = 10
+b = 20
+# Addition
+print("Addition:", a + b)
+# Subtraction
+print("Subtraction:", a - b)
+# Multiplication
+print("Multiplication:", a * b)
+# Division
+print("Division:", a / b)
+# Modulus   
+print("Modulus:", a % b)
+# Exponentiation
+print("Exponentiation:", a ** b)
+# Floor Division
+print("Floor Division:", a // b)
