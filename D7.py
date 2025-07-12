76bd9ac249a03c8612cd9ee9bc51457f155bb0f9
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-12 18:42:34 +0530

    Create D7.py

---
+++ D7.py
@@ -0,0 +1,23 @@
+# Assignment operators
+# Assignment
+a = 5
+
+# Add and assign
+a += 3  # a = a + 3
+print("After Add and Assign:", a)
+# Subtract and assign
+a -= 2  # a = a - 2
+print("After Subtract and Assign:", a)
+# Multiply and assign
+a *= 4  # a = a * 4
+print("After Multiply and Assign:", a)
+# Divide and assign
+a /= 2  # a = a / 2
+print("After Divide and Assign:", a)
+# Modulus and assign
+a %= 3  # a = a % 3
+print("After Modulus and Assign:", a)
+# Exponentiation and assign
+a **= 2  # a = a ** 2
+print("After Exponentiation and Assign:", a)
+
