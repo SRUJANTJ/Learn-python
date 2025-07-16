40f4b7ce30b233255ef184227ba3813ff8c8fe87
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-16 19:50:43 +0530

    Update C1.py

--- Conditionals/C1.py
+++ Conditionals/C1.py
@@ -6,9 +6,30 @@
 
 
 
-    # else Statement
-if(10 < 5):
-        print("10 is less than 5")
-else:
-    print("10 is not greater than 5")
+    # if-else Statement
+
+
+
+
+
+
+
+# if(10 < 5):
+#         print("10 is less than 5")
+# else:
+#     print("10 is not greater than 5")
     
+
+
+
+
+
+
+ # if-else Statement
+x = 10
+if x > 10:
+  print("x is greater than 10")
+elif x == 10:
+  print("x is equal to 10")  # This will execute
+else:
+  print("x is less than 10")
