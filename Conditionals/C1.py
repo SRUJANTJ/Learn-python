082125ee12bb05aac95ca561e487d139851f5f4e
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-16 19:49:38 +0530

    Create C1.py

---
+++ Conditionals/C1.py
@@ -0,0 +1,14 @@
+# if Statement
+
+# if(10>5):
+#     print("10 is greater than 5")
+
+
+
+
+    # else Statement
+if(10 < 5):
+        print("10 is less than 5")
+else:
+    print("10 is not greater than 5")
+    
