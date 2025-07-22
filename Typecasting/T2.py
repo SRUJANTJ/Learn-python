02f7711e9fc0e55dad2195958cbca921babe8cb1
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-22 14:57:17 +0530

    Update T2.py

--- Typecasting/T2.py
+++ Typecasting/T2.py
@@ -24,3 +24,24 @@ def add_numbers(a: int, b: int) -> int:
 
 result = add_numbers(3, 5)
 print(result)  # Output: 8
+
+
+
+
+
+
+
+
+# Explicit Casting to Set
+numbers_list = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = set(numbers_list)  # Converts list to set, removing duplicates
+print(unique_numbers)  # Output: {1, 2, 3, 4, 5}
+
+
+# Implicit Type Conversion Example
+# Done by Python automatically
+# when performing operations between different numeric types
+num_int = 10
+num_float = 5.5
+result = num_int + num_float  # Python automatically converts num_int to float
+print(result)  # Output: 15.5
