aafe835e2e176b6111f19cfa702089d17ec19e82
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-23 13:32:12 +0530

    Create F1.py

---
+++ Functions/F1.py
@@ -0,0 +1,50 @@
+# Defining a function
+# 'def' is used to define the function
+# 'function()' here is the name of the function, but it can be any valid name
+# The parentheses '()' mean it's a function and can hold arguments
+
+def function():
+    print('hello')
+
+# Calling the function
+function()
+
+
+
+
+
+
+
+# Examples of Python built-in functions
+
+# print() - Outputs to the console
+print('Hello, World!')
+
+# len() - Returns the length of an object
+my_list = [1, 2, 3, 4]
+print(len(my_list))  # Output: 4
+
+# type() - Returns the type of a variable
+num = 10
+print(type(num))  # Output: <class 'int'>
+
+# sum() - Returns the sum of an iterable
+numbers = [1, 2, 3, 4]
+print(sum(numbers))  # Output: 10
+
+# max() - Returns the largest item in an iterable
+print(max(numbers))  # Output: 4
+
+# min() - Returns the smallest item in an iterable
+print(min(numbers))  # Output: 1
+
+# sorted() - Returns a sorted list from the specified iterable
+print(sorted(numbers))  # Output: [1, 2, 3, 4]
+
+# str() - Converts a value to a string
+value = 123
+print(str(value))  # Output: '123'
+
+# abs() - Returns the absolute value of a number
+negative_number = -5
+print(abs(negative_number))  # Output: 5
