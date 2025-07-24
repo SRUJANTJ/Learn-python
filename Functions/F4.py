fcdc738a2e0dd456f68f001362ecf3f3de38daf9
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-07-24 19:58:26 +0530

    Update F4.py

--- Functions/F4.py
+++ Functions/F4.py
@@ -20,9 +20,24 @@ greet('Bob')  # Output: Hello, Bob!
 
 # Arbitrary positional arguments:
 # Use *args to collect any number of positional arguments into a tuple.
+# use when Don't know how many arguments will be passed to the function.
 
 
 def my_function(*args):
     for arg in args:
        print(arg)
 my_function(1, 2, 3, 4, 5)  # Output: 1 2 3 4 5
+
+
+
+
+
+
+# Use **kwargs to collect any number of keyword arguments into a dictionary.
+# use when Don't know how many keyword arguments will be passed to the function.
+
+def my_function(**kwargs):
+    for key, value in kwargs.items():
+       print(key, value)
+my_function(name="Alice", age=30, city="New York")
+  # Output: name Alice age 30 city New York
