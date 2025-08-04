4ffb6e03fbbf14801df5914b0ddf71f3ce57e203
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-08-04 09:18:54 +0530

    Create NumPy.py

---
+++ Libraries/NumPy.py
@@ -0,0 +1,16 @@
+# Why numpy
+# Machine Learning deals with large datasets and mathematical computations (like matrix multiplication, dot products, etc.).
+
+# Python’s built-in lists are slow because they:
+
+# Store elements as generic Python objects (not optimized for numbers).
+
+# Lack built-in vectorized math operations.
+
+# NumPy solves this by:
+
+# Storing data in contiguous memory blocks (fast access).
+
+# Using C/Fortran-optimized backend for computation.
+
+# Allowing vectorized operations (no slow Python loops).
