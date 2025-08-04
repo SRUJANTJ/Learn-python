e388e53fc4bc86fb38c63d6210e12ab63a8d6df4
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-08-04 18:45:49 +0530

    Update NumPy.py

--- Libraries/NumPy.py
+++ Libraries/NumPy.py
@@ -14,3 +14,31 @@
 # Using C/Fortran-optimized backend for computation.
 
 # Allowing vectorized operations (no slow Python loops).
+
+# Creating Arrays
+import numpy as np
+
+arr1=np.array([1, 2, 3, 4, 5])  # Create a 1D array
+
+arr2= np.array([[1, 2, 3], [4, 5, 6]])  # Create a 2D array
+
+# Creating arrays with special functions
+zeros = np.zeros((3, 3))  # 3x3 matrix of zeros
+ones = np.ones((2, 4))    # 2x4 matrix of ones
+rand = np.random.rand(3, 2) # Random numbers between 0 and 1
+
+print(arr1)
+print(arr2)
+print(zeros)
+print(ones)
+print(rand)
+
+
+
+# Array Attributes
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr.shape)   # (2, 3) → 2 rows, 3 columns
+print(arr.ndim)    # 2 dimensions
+print(arr.size)    # Total elements
+print(arr.dtype)   # Data type (int64, float32, etc.)
+
