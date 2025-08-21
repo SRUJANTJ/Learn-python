427da52168f6b33d9acd3e6bf38ba9d2ae150a9f
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-08-21 14:47:01 +0530

    Create m1.py

---
+++ Libraries/Matplotlib/m1.py
@@ -0,0 +1,37 @@
+# It helps you create 2D graphs & plots (line plots, bar charts, histograms, scatter plots, etc.).
+
+# In ML, it’s mainly used for visualizing datasets, distributions, model results, and training progress.
+# 🔹 Why Use Matplotlib in ML?
+
+# Understand your data before training (Exploratory Data Analysis – EDA).
+
+# Example: See how features are distributed with histograms.
+
+# Visualize relationships between variables.
+
+# Example: Scatter plots to check correlation.
+
+# Track model performance.
+
+# Example: Plot training vs validation accuracy/loss.
+
+# Communicate results clearly.
+
+# Example: Bar plots for feature importance.
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Create a line plot
+plt.plot(x, y, label="sin(x)", color="blue")
+
+
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Basic Line Plot")
+
+# Show legend and graph
+plt.legend()
+plt.show()
