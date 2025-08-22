3400e783c42f81a7999cc32eca995e27be0a01e5
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-08-22 22:30:11 +0530

    Update m2.py

--- Libraries/Matplotlib/m2.py
+++ Libraries/Matplotlib/m2.py
@@ -3,7 +3,7 @@
 # A line plot connects data points with lines.
 # In ML, we often track how loss and accuracy change as training progresses (over epochs).
 import matplotlib.pyplot as plt 
-
+import numpy as np
 epochs = [1, 2, 3, 4, 5]
 train_loss = [0.9, 0.7, 0.5,0.3,0.2]
 
@@ -11,4 +11,16 @@ plt.plot(epochs , train_loss, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Training Loss')
 plt.title('Training Loss over Epochs')
+plt.show()
+
+
+
+# Scatter Plot – For feature relationships
+x = np.random.rand(50)
+y = x * 2 + np.random.rand(50)
+
+plt.scatter(x, y, color="red")
+plt.xlabel("Feature X")
+plt.ylabel("Feature Y")
+plt.title("Feature Relationship")
 plt.show()
