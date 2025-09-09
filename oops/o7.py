5f91db28e881edf2e0a758c9e83d63f3c1410c27
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-09-09 17:50:24 +0530

    Update o7.py

--- oops/o7.py
+++ oops/o7.py
@@ -40,3 +40,33 @@ class Dog(Animal):
 my_dog = Dog("Buddy")
 print(my_dog.bark())  # Output: Buddy says Woof!
 print(my_dog._make_sound())  # Output: Some sound (not recommended)
+
+
+
+
+
+
+
+
+# Private
+# Private members are indicated by a double underscore prefix (__). They are intended to be inaccessible from outside the class. Python uses name mangling to make it harder to access private attributes from outside the class.
+
+class BankAccount:
+    def __init__(self, balance=0):
+        self.__balance = balance  # Private attribute
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount()
+account.deposit(100)
+print(account.get_balance())  # Output: 100
+# print(account.__balance)  # AttributeError: 'BankAccount' object has no attribute '__balance'
