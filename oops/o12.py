b65019e4e9f1c545750fc25de86bae1115ddc217
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-09-20 18:06:18 +0530

    Update o12.py

--- oops/o12.py
+++ oops/o12.py
@@ -19,3 +19,19 @@ child_object.child_method()
 #Output 
 #This is the child method.
 #This is the parent method.
+
+
+
+# Magic/Dunder Methods
+# Magic methods, also called dunder (short for “double underscore”) methods, in Python are special methods with names that start and end with double underscores (e.g., __init__, __str__, etc.). They enable Python classes to have custom behaviors when using built-in functions and operators.
+
+# __init__ method
+# The init method is a special method that is automatically invoked when you create a new instance of a class. This method is responsible for setting up the object’s initial state, and it is where you would typically define any instance variables that you need. Also called "constructor", we have discussed this method already
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+p = Person("Alice")
+print(p.name)  # Output: Alice
