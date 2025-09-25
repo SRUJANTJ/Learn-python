29cc6b8d84139f9fd8b639bcdaea4ae72161ad90
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-09-25 17:21:58 +0530

    Update o14.py

--- oops/o14.py
+++ oops/o14.py
@@ -19,3 +19,32 @@ cat = Cat()  # Creating an instance of the Cat class
 
 print(dog.sound())  # Output: Bark!  # Calling the overridden method for dog
 print(cat.sound())  # Output: Meow!  # Calling the overridden method for cat
+
+
+
+
+
+
+
+
+
+# Operator Overloading
+# Operator Overloading in Python allows you to define custom behavior for operators (like +, -, *, etc.) when they are applied to objects of a class. This is done by defining special methods in the class, known as magic methods or dunder methods (because they start and end with double underscores, like __add__, __sub__, etc.).
+print("\nOperator Overloading Example:\n")
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    # Overloading the + operator
+    def __add__(self, other):
+        return self.value + other.value
+
+# Create instances of the Number class
+num1 = Number(5)
+num2 = Number(10)
+
+# Using the overloaded + operator
+result = num1 + num2  # Calls the __add__ method
+
+print(result)  # Output: 15
