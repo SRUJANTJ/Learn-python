f7f85caab9794ac25a75bcf758f339e999010bd7
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2025-10-22 09:52:46 +0530

    Create it9.py

---
+++ Iterators/it9.py
@@ -0,0 +1,24 @@
+# Custom Data Structures
+# You can create custom data structures that support iteration, allowing you to define how the data is accessed.
+
+
+class MyRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.current < self.end:
+            value = self.current
+            self.current += 1
+            return value
+        else:
+            raise StopIteration
+
+# Usage
+for num in MyRange(1, 5):
+    print(num)  # Outputs 1, 2, 3, 4
